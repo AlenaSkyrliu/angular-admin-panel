794f0beeedc30f21923641f7284470d1cd8d6346
Alenka <AlenaSkyrliu@github.com>
2020-05-18 00:04:24 +0300

    add menu

--- src/dashboard/src/app/shared/shared.module.ts
+++ src/dashboard/src/app/shared/shared.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 
@@ -13,7 +19,14 @@ import {SidebarComponent} from './components/sidebar/sidebar.component';
     SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatToolbarModule,
+    FlexModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
   ],
   exports: [
     HeaderComponent,
