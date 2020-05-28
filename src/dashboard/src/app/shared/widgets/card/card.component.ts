19e36052a4f017a26db9968a382c541a74fe0bd3
Alenka <AlenaSkyrliu@github.com>
2020-05-28 15:33:36 +0300

    add new elements

--- src/dashboard/src/app/shared/widgets/card/card.component.ts
+++ src/dashboard/src/app/shared/widgets/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-card',
@@ -6,10 +8,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  Highcharts = Highcharts;
+  chartOptions = {};
+  @Input() label: string;
+  @Input() total: string;
+  @Input() percentage: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+        backgroundColor: null,
+        borderWidth: 0,
+        margin: [2, 2, 2, 2],
+        height: 60,
+      },
+      title: {
+        text: null
+      },
+      subtitle: {
+        text: null
+      },
+
+      tooltip: {
+        split: true,
+        outside: true,
+      },
+      legend: {
+        enabled: false,
+      },
+      credits: {
+        enabled: false,
+      },
+      exporting: {
+        enabled: false,
+      },
+      yAxis: {
+        labels: {
+          enabled: false,
+        },
+        title: {
+          text: null,
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      xAxis: {
+        labels: {
+          enabled: false,
+        },
+        title: {
+          text: null,
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      series: [{
+        data: [71, 78, 39, 67]
+      }]
+    };
+
+    HC_exporting(Highcharts);
+
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
   }
 
 }
